86680a7df10ebeec98fa81a74818d52d05166306
AndreyCoder <andreyme05@gmail.com>
2022-08-28 17:51:41 -0600

    open grap

--- src/app/App.js
+++ src/app/App.js
@@ -5,13 +5,15 @@ import s from './App.module.css'
 function App() {
 
   const [allPokemons, setAllPokemons] = useState([])
-  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=10')
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=6')
+
 
   const getAllPokemons = async () => {
     const res = await fetch(loadMore)
     const data = await res.json()
 
     setLoadMore(data.next)
+
 
     function createPokemonObject(result) {
       result.forEach(async (pokemon) => {
@@ -29,13 +31,17 @@ function App() {
   }, [])
 
 
+
+
+
+
   return (
     <div className={s.app}>
 
       <div className='s.main'>
         <Pokemos
           allPokemons={allPokemons}
-          getAllPokemons={getAllPokemons}
+          getAllPokemons={setAllPokemons}
         />
       </div>
 
